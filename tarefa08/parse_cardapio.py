beb1d8411f33eec5318b481852852de17e26adad
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-04-24 21:45:23 -0300

    tarefa08

--- tarefa08/parse_cardapio.py
+++ tarefa08/parse_cardapio.py
@@ -1,8 +1,42 @@
 from xml.dom.minidom import parse
-dom = parse(' ')
+dom = parse('../tarefa04/cardapio.xml') 
 cardapio = dom.documentElement
 pratos = cardapio.getElementsByTagName('prato')
+while True:
+    print("----- Pratos disponíveis: ------")
+    for prato in pratos:
+        nome = prato.getElementsByTagName("nome")[0].firstChild.nodeValue.strip()
+        id = prato.getAttribute("id")
+        print(F'{id} | {nome}')
+    print("--------------------------------")
+    input_id = int(input("Digite o id do prato para saber mais detalhes: "))
+    print("--------------------------------")
 
-for prato in pratos:
-    i = input(f'nome do prato: {prato.getAttribute("nome")}, id do prato: {prato.getAttribute("id")}')
-    print(i)
+    if input_id <= len(pratos):
+        prato_selecionado = pratos[int(input_id)-1]
+        descricao = prato_selecionado.getElementsByTagName("descricao")[0].firstChild.nodeValue.strip()
+        ingrediente = prato_selecionado.getElementsByTagName("ingrediente")[0].firstChild.nodeValue.strip()
+        preco = prato_selecionado.getElementsByTagName("preco")[0].firstChild.nodeValue.strip()
+        calorias = prato_selecionado.getElementsByTagName("calorias")[0].firstChild.nodeValue.strip()
+        tempo_preparo = prato_selecionado.getElementsByTagName("tempoPreparo")[0].firstChild.nodeValue.strip()
+        lista_ingredientes = []
+
+        for x in prato_selecionado.getElementsByTagName("ingrediente"):
+            ingrediente = x.firstChild.nodeValue.strip()
+            lista_ingredientes.append(ingrediente)
+        print(f"Descrição: {descricao}")
+        print("Ingredientes:")
+
+        for ing in lista_ingredientes:
+            print(f"- {ing}")
+        print(f"Preço: {preco}")
+        print(f"Calorias: {calorias}")
+        print(f"Tempo de preparo: {tempo_preparo}")
+        print("--------------------------------")
+
+    else:    
+        print("Id inválido.")
+    final = input("Deseja consultar outro prato? (s/n): ")
+    if final != 's':
+        break
+
