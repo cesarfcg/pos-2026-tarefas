a2f5625202de7a5eebe76110660f2b3626406653
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-05-15 22:58:42 -0300

    abonitando

--- tarefa11/cli.py
+++ tarefa11/cli.py
@@ -8,62 +8,83 @@ while True:
     "5 - Deletar usuário\n"
     "6 - Sair\n")
     if opcoes == "1":
-        user.listar()
-    elif opcoes == "2":
-        new_user = { "name": input("Digite o nome do user"), 
-                    "username": input("Digite o username do user"), 
-                    "email": input("Digite o email do user"),
-                     "address": {
-                        "street": input("Digite a rua do user"),
-                        "suite": input("Digite a suite do user"),
-                        "city": input("Digite a cidade do user"),
-                        "zipcode": input("Digite o zipcode do user"),
-                        "geo": {
-                            "lat": input("Digite a latitude do user"),
-                            "lng": input("Digite a longitude do user")
-                        },
-                        "phone": input("Digite o telefone do user"),
-                        "website": input("Digite o website do user"),
-                        "company": {
-                               "name": input("Digite o nome da empresa do user"),
-                               "catchPhrase": input("Digite a catchPhrase da empresa do user"),
-                               "bs": input("Digite o bs da empresa do user")
-                    }}
-                    }
+        print(user.listar())
+
+    if opcoes == "2":
+        new_user = {}
+        new_user["name"] = input("Digite o nome do usuário: ")
+        new_user["username"] = input("Digite o username do usuário: ")
+        new_user["email"] = input("Digite o email do usuário: ")
+        new_user["address"] = {
+            "street": input("Digite a rua do usuário: "),
+            "suite": input("Digite a suite do usuário: "),
+            "city": input("Digite a cidade do usuário: "),
+            "zipcode": input("Digite o zipcode do usuário: "),
+            "geo": {
+                "lat": input("Digite a latitude do usuário: "),
+                "lng": input("Digite a longitude do usuário: ")
+            }
+        }
+        new_user["phone"] = input("Digite o telefone do usuário: ")
+        new_user["website"] = input("Digite o website do usuário: ")
+        new_user["company"] = {
+            "name": input("Digite o nome da empresa do usuário: "),
+            "catchPhrase": input("Digite a catchPhrase da empresa do usuário: "),
+            "bs": input("Digite o bs da empresa do usuário: ")
+        }
         
-        user.criar(new_user)
-    elif opcoes == "3":
+        criou = user.criar(new_user)
+        if criou:
+            print("Usuário criado com sucesso!")
+        else:
+            print("Falha ao criar usuário.")
+
+    
+
+    if opcoes == "3":
         user_id = input("Digite o ID do usuário que deseja ler: ")
-        user.ler(user_id)
-    elif opcoes == "4":
+        print(user.ler(user_id))
+    
+    if opcoes == "4":
         user_id = input("Digite o ID do usuário que deseja atualizar: ")
-        put_user = { "name": input("Digite o novo nome do user"), 
-                         "username": input("Digite o novo username do user"), 
-                         "email": input("Digite o novo email do user"),
-                          "address": {
-                             "street": input("Digite a nova rua do user"),
-                             "suite": input("Digite a nova suite do user"),
-                             "city": input("Digite a nova cidade do user"),
-                             "zipcode": input("Digite o novo zipcode do user"),
-                             "geo": {
-                                 "lat": input("Digite a nova latitude do user"),
-                                 "lng": input("Digite a nova longitude do user")
-                             },
-                             "phone": input("Digite o novo telefone do user"),
-                             "website": input("Digite o novo website do user"),
-                             "company": {
-                                    "name": input("Digite o novo nome da empresa do user"),
-                                    "catchPhrase": input("Digite a nova catchPhrase da empresa do user"),
-                                    "bs": input("Digite o novo bs da empresa do user")
-                                }
-                         }}
-        user.atualizar(user_id, put_user)
-    elif opcoes == "5":
+        put_user = {}
+        put_user["name"] = input("Digite o nome do usuário: ")
+        put_user["username"] = input("Digite o username do usuário: ")
+        put_user["email"] = input("Digite o email do usuário: ")
+        put_user["address"] = {
+            "street": input("Digite a rua do usuário: "),
+            "suite": input("Digite a suite do usuário: "),
+            "city": input("Digite a cidade do usuário: "),
+            "zipcode": input("Digite o zipcode do usuário: "),
+            "geo": {
+                "lat": input("Digite a latitude do usuário: "),
+                "lng": input("Digite a longitude do usuário: ")
+            }
+        }   
+        put_user["phone"] = input("Digite o telefone do usuário: ")
+        put_user["website"] = input("Digite o website do usuário: ")
+        put_user["company"] = {
+            "name": input("Digite o nome da empresa do usuário: "),
+            "catchPhrase": input("Digite a catchPhrase da empresa do usuário: "),
+            "bs": input("Digite o bs da empresa do usuário: ")
+        }
+        
+        atualizou = user.atualizar(user_id, put_user)
+        if atualizou:
+            print("Usuário atualizado com sucesso!")
+        else:
+            print("Falha ao atualizar usuário.")
+
+
+    if opcoes == "5":
         user_id = input("Digite o ID do usuário que deseja deletar: ")
         confirmacao = input(f"Tem certeza que deseja deletar o usuário com ID {user_id}? (s/n): ")
         if confirmacao == 's':
-            user.deletar(user_id)
+            deletou = user.deletar(user_id)
+            if deletou:
+                print("Usuário deletado com sucesso!")
+            else:
+                print("Falha ao deletar usuário.")
         else:
             print("Operação de deleção cancelada.") 
-    else:
-        print("Opção inválida. Por favor, escolha uma opção válida.")
+    
--- tarefa11/users_wrapper.py
+++ tarefa11/users_wrapper.py
@@ -5,27 +5,34 @@ def listar():
     response = requests.get(API_URL)
     inf = response.json()
     if response.status_code == 200:
-        print(inf)
+        return inf
+    else:        
+        return False 
 
 def ler(user_id):
     response = requests.get(f"{API_URL+"/"+user_id}")
     if response.status_code == 200:
-        print (response.json())
-
+        return response.json()
+    else:
+        return False
 def criar(content):
     response = requests.post(API_URL, json=content)
     if response.status_code == 201:
-        print("User criado",response.json())
+        return response.json()
+    else:
+        return False
 
 def deletar(user_id):
     response = requests.delete(f"{API_URL+"/"+user_id}")
     if response.status_code == 200:
-        print(f"User {user_id} foi deletado")
+        return True
+    else:        
+        return False
 
 def atualizar(user_id, content):
     response = requests.put(f"{API_URL+"/"+user_id}", json=content)
-    u = response.json()
+    usu_novo = response.json()
     if response.status_code == 200:
-        print(f"Usuário id {u['id']} atualizado")
-    else:
-        print("erro")
+        return usu_novo
+    else:        
+        return False
