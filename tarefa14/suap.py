ad1233c7a757ebe60b94115d9037352b6e62206d
Fernando César <fernandocesar4141@gmail.com>
2026-06-19 16:50:11 -0300

    Clean up response output in suap.py
    Removed print statements for response text and JSON.

--- tarefa14/suap.py
+++ tarefa14/suap.py
@@ -24,8 +24,6 @@ periodo_letivo = int(input("Digite o periodo letivo:"))
 URL = f"/api/ensino/meu-boletim/{ano_letivo}/{periodo_letivo}/"
 response = requests.get(api_url+URL, headers=headers)
 
-print(response.text)
-print(response.json())
 disciplinas = response.json()["results"]
 for x in disciplinas:
     print(f"{x["disciplina"]}\n - nota etapa 1: {x["nota_etapa_1"]["nota"]}\n- nota etapa 2: {x["nota_etapa_2"]["nota"]}\n - nota etapa 3: {x["nota_etapa_3"]["nota"]}\n - nota etapa 4: {x["nota_etapa_4"]["nota"]}")
